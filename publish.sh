0f455467aa3e3468ff573e41b80ec62e8e8e60e6
Daniel Brain <dbrain@paypal.com>
2017-06-05 19:01:00 -0700

    Update publish script

--- publish.sh
+++ publish.sh
@@ -2,8 +2,18 @@
 
 set -e;
 
-rm -rf node_modules
-npm install
+if ! git diff-files --quiet; then
+    echo "Can not publish with unstaged uncommited changes";
+    exit 1;
+fi;
+
+if ! git diff-index --quiet --cached HEAD; then
+    echo "Can not publish with staged uncommited changes";
+    exit 1;
+fi;
+
+rm -rf node_modules/cross-domain-safe-weakmap
+npm install cross-domain-safe-weakmap
 
 gulp build;
 
