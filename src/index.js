9ed15b484c332fc4dd4f444bb026f34396924bd8
Daniel Brain <dbrain@paypal.com>
2017-08-03 14:29:21 -0700

    Add isWindow

--- src/index.js
+++ src/index.js
@@ -766,3 +766,16 @@ export function onCloseWindow(win : any, callback : Function, delay : number = 1
         }
     };
 }
+
+export function isWindow(obj : Object) {
+
+    try {
+        if (obj && obj.self === obj) {
+            return true;
+        }
+    } catch (err) {
+        // pass
+    }
+
+    return false;
+}
