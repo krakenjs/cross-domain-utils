cb9f34a75c72870efe8292ad8ff89daf30c2bf8c
Daniel Brain <dbrain@paypal.com>
2018-04-19 14:37:46 -0700

    Add postMessage function type

--- src/types.js
+++ src/types.js
@@ -11,7 +11,8 @@ export type CrossDomainWindowType = {|
     frames : Array<CrossDomainWindowType>,
     opener ? : CrossDomainWindowType,
     parent : CrossDomainWindowType,
-    length : number
+    length : number,
+    postMessage : (string, string) => void
 |};
 
 export type SameDomainWindowType = Object & {
