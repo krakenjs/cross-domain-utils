324167ddbf499a8f572d20281f19c67f8946d421
Abdul Rehman <abdul-rehman@myseneca.ca>
2020-10-03 21:47:13 -0400

    added unit test for array of regexes

--- test/tests/stringifyDomainPattern.js
+++ test/tests/stringifyDomainPattern.js
@@ -6,19 +6,32 @@ import { stringifyDomainPattern } from '../../src';
 
 describe('stringifyDomainPattern test cases ', () => {
 
-    it('should stringify a single regex expression', () => {
+    it('should stringify a single regex expression to RegExp', () => {
 
         const pattern = /[a-zA-Z0-9]{1,5}/;
-        const domainPattern = stringifyDomainPattern(pattern);
+        const domainPatternStringified = stringifyDomainPattern(pattern);
 
         const expected = `RegExp(${ pattern.toString() })`;
 
-        if (typeof domainPattern !== 'string') {
-            throw new TypeError(`Expected domainPattern to be string, instead got ${ typeof domainPattern }`);
+        if (typeof domainPatternStringified !== 'string') {
+            throw new TypeError(`Expected domainPattern to be string, instead got ${ typeof domainPatternStringified }`);
         }
 
-        expect(domainPattern).to.equal(expected);
+        expect(domainPatternStringified).to.equal(expected);
 
+    });
+
+    it('should stringify an array of domain patterns to RegExp', () => {
+
+        const p1 = '/[a-zA-Z0-9]{1,5}/';
+        const p2 = '/\\.[a-zA-Z]{2,}$/';
+        const domainPatternsArray = [ p1, p2 ];
+
+        const domainPatternsArrayStringified = stringifyDomainPattern(domainPatternsArray);
+        
+        const expected = `RegExp(${ p1 } | ${ p2 })`;
+
+        expect(domainPatternsArrayStringified).to.equal(expected);
     });
     
 });
