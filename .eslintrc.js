f00a02d492660a7acade12f3af6fbdc98797f1ab
Westin Wrzesinski <westin.wrzesinski@gmail.com>
2022-04-08 16:16:10 -0500

    chore: move devDependencies to @krakenjs scope

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 /* @flow */
 
 module.exports = {
-    'extends': './node_modules/grumbler-scripts/config/.eslintrc.js',
+    'extends': './node_modules/@krakenjs/grumbler-scripts/config/.eslintrc.js',
 
     'globals': {
         '__TEST__': true
