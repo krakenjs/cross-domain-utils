401fd0c53a1c19024c65504ea02ee6b3177a7cd7
Westin Wrzesinski <westin.wrzesinski@gmail.com>
2022-04-12 20:12:00 -0500

    chore: vite build task

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,32 @@
+/* eslint-disable eslint-comments/disable-enable-pair */
+/* eslint-disable spaced-comment */
 /// <reference types="vitest" />
 
 // Configure Vitest (https://vitest.dev/config/)
 
-import { defineConfig } from 'vite'
+import path from 'path';
 
+import { defineConfig } from 'vite';
+
+
+// eslint-disable-next-line import/no-default-export
 export default defineConfig({
-  test: {
+    build: {
+        lib: {
+            entry:    path.resolve(__dirname, 'src/index.ts'),
+            name:     'crossDomainUtil',
+            fileName: (format) => `cross-domain-utils.${ format }.js`,
+            formats:  [ 'es', 'umd' ]
+        },
+        sourcemap:     true,
+        rollupOptions: {
+            ouput: {
+                preserveModules: true
+            }
+        }
+    },
+    test: {
     /* for example, use global to avoid globals imports (describe, test, expect): */
     // globals: true,
-  },
-})
+    }
+});
