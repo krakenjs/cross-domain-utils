25113e700019de36cdfd1421b65fc352566545dd
Westin Wrzesinski <westin.wrzesinski@gmail.com>
2022-07-04 19:56:41 -0500

    chore: wip replace grumbler-scripts

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,28 @@
 module.exports = {
-  extends: "@krakenjs/grumbler-scripts/config/.babelrc-browser",
-  presets: ["@krakenjs/grumbler-scripts/config/flow-ts-babel-preset"],
+  presets: [
+    [
+      "@babel/env",
+      {
+        targets: {
+          ie: 11,
+          chrome: 27,
+          firefox: 30,
+          safari: 7,
+          opera: 23,
+        },
+        loose: true,
+        modules: false,
+        exclude: ["transform-regenerator"],
+      },
+    ],
+    "@babel/preset-typescript",
+  ],
+  plugins: [
+    ["@babel/plugin-syntax-dynamic-import", { loose: true }],
+    ["@babel/plugin-proposal-class-properties", { loose: true }],
+    [
+      "@babel/plugin-transform-runtime",
+      { corejs: false, helpers: true, regenerator: false, useESModules: true },
+    ],
+  ],
 };
--- vite.config.js
+++ vite.config.js
@@ -2,27 +2,52 @@
 /* eslint-disable spaced-comment */
 /// <reference types="vitest" />
 
-// Configure Vitest (https://vitest.dev/config/)
-
+// Configure Vitest (https://vitest.dev/config/) import path from "path";
 import path from "path";
-
 import { defineConfig } from "vite";
+
+import packageJson from "./package.json";
+
+/**
+ * Take the name from package.json and grab the last part
+ * eg getPackageName "@krakenjs/foobar" -> "foobar"
+ */
+const getPackageName = () => {
+  const npmName = packageJson.name;
+  return npmName.split("/").slice(-1)[0];
+};
+
+const getPackageNameCamelCase = () => {
+  try {
+    return getPackageName().replace(/-./g, (char) => char[1].toUpperCase());
+  } catch (err) {
+    throw new Error("Name property in package.json is missing.");
+  }
+};
+
+const fileName = {
+  es: `${getPackageName()}.mjs`,
+  cjs: `${getPackageName()}.cjs`,
+  iife: `${getPackageName()}.iife`,
+};
 
 // eslint-disable-next-line import/no-default-export
 export default defineConfig({
   build: {
     lib: {
       entry: path.resolve(__dirname, "src/index.ts"),
-      name: "crossDomainUtil",
-      fileName: (format) => `cross-domain-utils.${format}.js`,
-      formats: ["es", "umd"],
+      name: getPackageNameCamelCase(),
+      fileName: (format) => `${getPackageNameCamelCase()}.${format}.js`,
+      formats: ["es", "cjs", "iife", "umd"],
     },
     sourcemap: true,
-    rollupOptions: {
-      ouput: {
-        preserveModules: true,
-      },
-    },
+    //rollupOptions: {
+    //ouput: {
+    //dir: "./dist/",
+    //preserveModules: true,
+    //preserveModulesRoot: 'src',
+    //},
+    //},
   },
   test: {
     /* for example, use global to avoid globals imports (describe, test, expect): */
